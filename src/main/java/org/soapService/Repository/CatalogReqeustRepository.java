e26ca4c00e6218590401d7fe934148127ebbf985
M Farrel Danendra Rachim <13521048@std.stei.itb.ac.id>
2023-11-12 21:19:19 +0700

    feat: Add catalogrequestrepository

--- src/main/java/org/soapService/Repository/CatalogReqeustRepository.java
+++ src/main/java/org/soapService/Repository/CatalogReqeustRepository.java
@@ -1,13 +1,30 @@
 package org.soapService.Repository;
 
+import org.soapService.Config.Database;
 import org.soapService.Domain.CatalogRequest;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.UUID;
+
 
 public class CatalogReqeustRepository implements BaseRepository<CatalogRequest> {
+
+    private static Connection conn = Database.getConnection();
     public int add(CatalogRequest data) throws SQLException {
-        return 0;
+        String query = "INSERT INTO catalog_requests(title, description, poster, trailer, category, uuid) VALUES (?, ?, ?, ?, ? ,?)";
+        PreparedStatement ps = conn.prepareStatement(query);
+        ps.setString(1, data.getTitle());
+        ps.setString(2, data.getDescription());
+        ps.setString(3, data.getPoster());
+        ps.setString(4, data.getTrailer());
+        ps.setString(5, data.getCategory());
+        ps.setString(6, UUID.randomUUID().toString());
+
+
+        return ps.executeUpdate();
     }
 
     public List<CatalogRequest> getAll() throws SQLException {
